3a71bd08a27e1b209ed4e029e35ea361b73b6c36
asandilenkala <asandilenkala@gmail.com>
2025-01-15 17:31:41 +0200

    Modified the database, and added comments and docstrings

---
+++ src/main/java/com/enviro/assessment/grad001/asandilenkala/service/WasteCategoryServiceInterface.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.asandilenkala.service;
+
+import com.enviro.assessment.grad001.asandilenkala.model.WasteCategory;
+
+import java.util.List;
+
+public interface WasteCategoryServiceInterface {
+    WasteCategory createCategory(WasteCategory category);
+    WasteCategory updateCategory(Long id, WasteCategory category);
+    void deleteCategory(Long id);
+    List<WasteCategory> getAllCategories();
+    WasteCategory getCategoryById(Long id);
+}
